11bab7660b5f6c46bc9161147868933b03609ea6
gabriel <gabrielsrosas@gmail.com>
2026-06-20 18:44:27 -0300

    Revert "us6 editar/deletar transacoes"
    This reverts commit 71d6bddbae2fef48a653db7887d7ab532de5e7b9.

--- easyfin-clean/src/core/repositories.py
+++ easyfin-clean/src/core/repositories.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List
 from .entities import Transacao, Categoria 
 from typing import List, Optional
 
@@ -15,15 +15,11 @@ class TransacaoRepositoryInterface(ABC):
     @abstractmethod
     def buscar_por_usuario(self, usuario_id: int, limite: int = 20) -> List[Transacao]:
         pass
-    
-    @abstractmethod
-    def buscar_por_id(self, transacao_id: int, usuario_id: int) -> Optional[Transacao]:
-        pass
 
     @abstractmethod
     def deletar(self, transacao_id: int, usuario_id: int) -> bool:
         pass
-        
+
 class CategoriaRepositoryInterface(ABC):
     @abstractmethod
     def buscar_por_usuario(self, usuario_id: int) -> List[Categoria]:
